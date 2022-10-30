a61741ea000d9dcafb1c837d771685a3afb8930b
Deependra Kumar <deependrarajpoot178@gmail.com>
2022-10-30 14:57:59 +0530

    added logger middleware

--- myApp.js
+++ myApp.js
@@ -1,12 +1,27 @@
+
 let express = require('express');
 let app = express();
- 
+
+
 const absolutePath = __dirname + '/public/style.css';
+
+//logger middleware
+app.use((req, res, next)=>{
+    let string  = req.path + " " + req.method + " - " + req.ip;
+    console.log(string);
+    next();
+})
 
 app.use('/public/style.css', express.static(absolutePath) );
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + "/views/index.html");
+});
+app.get('/json', (req, res)=>{
+    res.json({
+        "message":process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json"
+    })
 })
+
 
 
 
